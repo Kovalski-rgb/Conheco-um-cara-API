631b724f80fbdf176d8dec42529e900757750da7
Kovalski <81390604+Kovalski-rgb@users.noreply.github.com>
2023-01-19 20:40:30 -0300

    Uploading basic server

---
+++ authServer/code/user/router.mjs
@@ -0,0 +1,61 @@
+import { getUser, login } from "./service.mjs";
+
+/**
+ * @openapi
+ * /users/login:
+ *   post:
+ *     summary: "Logs in the user"
+ * 
+ *     tags:
+ *       - "auth"
+ *     
+ *     operationId: user_login
+ *     x-eov-operation-handler: router
+ * 
+ *     requestBody:
+ *       description: Login information
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UsernamePassword" 
+ * 
+ *     responses:
+ *       '200':
+ *         description: "User logged in"
+ *       '400':
+ *         description: "Invalid data provided"
+ *       '401':
+ *         description: "Login failed"
+ */
+export async function user_login(req, res, _) {
+  const user = await login(req.body);
+  return user ? res.json(user) : res.sendStatus(401);
+}
+
+/**
+ * @openapi
+ * 
+ * /users/{id}:
+ *   get:
+ *     summary: "Retrieves user information"
+ * 
+ *     tags:
+ *       - "profile"
+ * 
+ *     parameters:
+ *       - $ref: "#/components/parameters/Id"
+ * 
+ *     operationId: get_user
+ *     x-eov-operation-handler: router
+ * 
+ *     responses:
+ *       '200':
+ *         description: "Returns the user"
+ *       '404':
+ *         description: "User not found"
+ */
+export async function get_user(req, res, _) {  
+  const user = await getUser(parseInt(req.params.id));
+  return user ? res.json(user) : res.sendStatus(404);  
+}
---
+++ authServer/code/user/service.mjs
@@ -0,0 +1,15 @@
+import { createToken } from "../jwt.mjs";
+import { loadByCredentials, loadById, } from "./repository.mjs";
+
+export async function login({username, password}) {
+    const user = await loadByCredentials(username, password);
+    if (user) return {
+        token: createToken(user),
+        ...user
+    };
+    return null;
+}
+
+export async function getUser(id) {
+    return loadById(id);
+}
