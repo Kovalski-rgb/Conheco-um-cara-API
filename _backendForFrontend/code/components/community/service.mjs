9c14acf4a8bb2ef825cbe7d32a8e380444b1a080
Carlos Suzuki <52303788+carlosuzuki@users.noreply.github.com>
2023-02-12 03:55:51 -0300

    feat: Implement create community in bff

--- _backendForFrontend/code/components/community/service.mjs
+++ _backendForFrontend/code/components/community/service.mjs
@@ -1,35 +1,69 @@
 import { newAxios } from "../../lib/network.mjs";
-import { filterCommunityByUserId } from "./repository.mjs";
 
-export async function getMyCommunities() {
-  const axiosUser = newAxios();
-  const responseUser = await axiosUser.get(
-    "http://localhost:3001/api/users/me"
-  ); //retorna inf do usuario
-
-  const axiosCommunity = newAxios();
-  const responseCommunity = await axiosCommunity.get(
-    "http://localhost:3003/api/communities"
-  ); //retorna lista com informações de todas comunidades
-
-  return await filterCommunityByUserId(responseCommunity.data, responseUser.Id); //seleciona apenas as comunidades do usuario e retorna ao BFF
+export async function getAllUserIds() {
+  return await getAllIdsFromUsers();
 }
 
-export async function getMyPosts() {
-  const axiosUser = newAxios();
-  const responseUser = await axiosUser.get(
-    "http://localhost:3001/api/users/me"
-  ); //retorna inf do usuario
+export async function createCommunity(userId, req) {
+  const axios = newAxios();
 
-  const axiosCommunity = newAxios();
-  const responseCommunity = await axiosCommunity.get(
-    "http://localhost:3003/api/communities"
-  ); //retorna lista com informações de todas comunidades
+  axios.interceptors.request.use(
+    config => {
+      config.headers.Authorization= req.header("Authorization");
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
 
-  return await filterCommunityByUserId(responseCommunity.data, responseUser.Id); //seleciona apenas as comunidades do usuario e retorna ao BFF
+  const response = await axios.post(
+    "http://localhost:3003/api/community/create",
+    {
+      userId: userId,
+      headers: { Authorization: req.header("Authorization") },
+      name: req.body.name,
+      description: req.body.description
+    }
+  );
+  return response.data;
+
 }
 
-export async function getMyMessages() {
+export async function listEveryUserFromCommunity(communityId) {
+  return await getAllUsersFromCommunity(communityId);
+}
 
+export async function listEveryComunity() {
+  return await getAllCommunities();
+}
+
+export async function listEveryComunityFromUser(id) {
+  return await getAllComunitiesFromUser(id);
+}
+
+export async function enterCommunity(userId, { name, code }) {
+  const exist = await checkCommunityCode(name, code);
+  if (exist) {
+    if (!(await checkIsUserAlreadyRegisteredInsideCommunity(name, userId))) {
+      await registerNewCommunityUser(name, userId);
+      return true;
+    }
   }
+  return false;
+}
 
+export async function removeUserFromCommunity(userId, communityName) {
+  if (
+    await checkIsUserAlreadyRegisteredInsideCommunity(communityName, userId)
+  ) {
+    console.log("registered");
+    return await deleteUserInsideCommunity(communityName, userId);
+  }
+  if (await checkIfUserIsModerator(communityName, userId)) {
+    console.log("mod");
+    return await deleteUserInsideCommunity(communityName, userId);
+  }
+  console.log("get out");
+  return false;
+}
